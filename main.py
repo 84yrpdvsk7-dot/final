51f1b7a552b664823d291bb55b5065af49289229
84yrpdvsk7-dot <84yrpdvsk7@privaterelay.appleid.com>
2026-05-11 18:06:24 -0600

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-signal_type = 0
-
-def on_forever():
+def show_signal():
+    basic.clear_screen()
     if signal_type == 0:
         basic.show_icon(IconNames.SMALL_HEART)
     elif signal_type == 1:
-        music.play(music.tone_playable(392, music.beat(BeatFraction.DOUBLE)),
-            music.PlaybackMode.UNTIL_DONE)
+        basic.show_icon(IconNames.EIGHTH_NOTE)
+        music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.IN_BACKGROUND)
     elif signal_type == 2:
-        basic.show_string("A")
+        basic.show_icon(IconNames.DIAMOND)
+def end_game():
+    global game_over
+    game_over = True
+    basic.clear_screen()
+    basic.show_string("Score")
+    basic.show_number(score)
+def check_score():
+    global score
+    # HEART -> Button A
+    if signal_type == 0:
+        if input.button_is_pressed(Button.A):
+            score += 1
+            basic.show_number(score)
+            basic.pause(300)
+        elif input.button_is_pressed(Button.B):
+            end_game()
+    elif signal_type == 1:
+        # MUSIC -> Button B
+        if input.button_is_pressed(Button.B):
+            score += 1
+            basic.show_number(score)
+            basic.pause(300)
+        elif input.button_is_pressed(Button.A):
+            end_game()
+    elif signal_type == 2:
+        # DIAMOND -> SHAKE
+        if input.is_gesture(Gesture.SHAKE):
+            score += 1
+            basic.show_number(score)
+            basic.pause(300)
+        elif input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B):
+            end_game()
+score = 0
+game_over = False
+signal_type = 0
+last_signal = -1
+
+def on_forever():
+    global last_signal
+    if not (game_over):
+        # Only redraw if signal changed
+        if signal_type != last_signal:
+            show_signal()
+            last_signal = signal_type
+        check_score()
+    basic.pause(50)
 basic.forever(on_forever)
 
 def on_every_interval():
     global signal_type
-    signal_type = randint(0, 2)
+    if not (game_over):
+        new_signal = randint(0, 2)
+        # Prevent same signal twice
+        while new_signal == signal_type:
+            new_signal = randint(0, 2)
+        signal_type = new_signal
 loops.every_interval(3000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,28 +1,73 @@
-let signal_type = 0
-let Score = 0
-basic.forever(function () {
+function show_signal () {
+    basic.clearScreen()
     if (signal_type == 0) {
         basic.showIcon(IconNames.SmallHeart)
-        basic.pause(1000)
-        basic.clearScreen()
-        if (signal_type == 0 && input.buttonIsPressed(Button.A)) {
-            Score += 1
+    } else if (signal_type == 1) {
+        basic.showIcon(IconNames.EighthNote)
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
+    } else if (signal_type == 2) {
+        basic.showIcon(IconNames.Diamond)
+    }
+}
+function end_game () {
+    game_over = true
+    basic.clearScreen()
+    basic.showString("Score")
+    basic.showNumber(score)
+}
+function check_score () {
+    // HEART -> Button A
+    if (signal_type == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            score += 1
+            basic.showNumber(score)
+            basic.pause(300)
+        } else if (input.buttonIsPressed(Button.B)) {
+            end_game()
         }
     } else if (signal_type == 1) {
-        music.play(music.tonePlayable(392, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
-        if (signal_type == 1 && input.buttonIsPressed(Button.B)) {
-            Score += 1
+        // MUSIC -> Button B
+        if (input.buttonIsPressed(Button.B)) {
+            score += 1
+            basic.showNumber(score)
+            basic.pause(300)
+        } else if (input.buttonIsPressed(Button.A)) {
+            end_game()
         }
     } else if (signal_type == 2) {
-        basic.showString("A")
-        basic.pause(1000)
-        basic.clearScreen()
-        if (signal_type == 2 && input.isGesture(Gesture.Shake)) {
-            Score += 1
+        // DIAMOND -> SHAKE
+        if (input.isGesture(Gesture.Shake)) {
+            score += 1
+            basic.showNumber(score)
+            basic.pause(300)
+        } else if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            end_game()
         }
     }
-    basic.clearScreen()
+}
+let new_signal = 0
+let score = 0
+let game_over = false
+let signal_type = 0
+let last_signal = -1
+basic.forever(function () {
+    if (!(game_over)) {
+        // Only redraw if signal changed
+        if (signal_type != last_signal) {
+            show_signal()
+            last_signal = signal_type
+        }
+        check_score()
+    }
+    basic.pause(50)
 })
 loops.everyInterval(3000, function () {
-    signal_type = randint(0, 2)
+    if (!(game_over)) {
+        new_signal = randint(0, 2)
+        // Prevent same signal twice
+        while (new_signal == signal_type) {
+            new_signal = randint(0, 2)
+        }
+        signal_type = new_signal
+    }
 })
